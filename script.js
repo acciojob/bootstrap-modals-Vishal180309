0cccde81ba652eb56e7fb4f3079f953199f2ca16
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-01-29 12:52:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+var modal = document.getElementById("newton-modal");
+var button = document.getElementById("open-modal");
+
+button.onclick = function() {
+    modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
